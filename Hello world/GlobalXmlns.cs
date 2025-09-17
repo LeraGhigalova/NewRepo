542e08a711599a40a55a4cb50f69ba7147e9ddf2
Валерия Жигалова <valerya.g06@mail.ru>
2025-09-17 18:45:21 +0500

    Старт ветвей разработки

---
+++ Hello world/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Hello_world")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Hello_world.Pages")]
