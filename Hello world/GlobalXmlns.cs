fd786c0d06711c9bfb10da2fe9a015d59363e27e
Валерия Жигалова <valerya.g06@mail.ru>
2025-09-24 19:59:49 +0500

    Задание 1 готово

--- Hello world/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Hello_world")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Hello_world.Pages")]
