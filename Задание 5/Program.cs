8d766bd05a09e76c3c8268afc33f258a9a07948c
Валерия Жигалова <valerya.g06@mail.ru>
2025-10-08 20:36:53 +0500

    Задание 5 готово

--- Задание 5/Program.cs
+++ Задание 5/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Задание_5
+namespace _5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var x = Calculate(2, 3) + Calculate(7, 8) + Calculate(1, 4);
+            Console.WriteLine(x);
         }
+
+        static double Calculate(double a, double b) =>
+            (a + Math.Tan(b)) / (a * a + b * b);
     }
 }
