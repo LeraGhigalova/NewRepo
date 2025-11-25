1c31386caf58c6304a3b5de1c5f797cea3bce340
Валерия Жигалова <valerya.g06@mail.ru>
2025-11-25 10:45:58 +0500

    Задание 10.5 готово

---
+++ Задание 10.5/Задание 10.5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_10._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число:");
+            long number = long.Parse(Console.ReadLine());
+
+            int sign = 1;
+            int result = 0;
+
+            while (number > 0)
+            {
+                int digit = (int)(number % 10);
+                result += sign * digit;
+                sign = -sign;
+                number /= 10;
+            }
+
+            Console.WriteLine($"Знакочередующая сумма цифр: {result}");
+        }
+    }
+}
